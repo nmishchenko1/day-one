36c06e6a9fd83ed47b309828ac381f2cafb57453
nmishchenko1 <nxmishchenko1@students.everettcc.edu>
2025-06-13 13:40:07 -0700

    edit

--- scrips/mobile-nav-2.js
+++ scrips/mobile-nav-2.js
@@ -6,7 +6,5 @@ function initNavToggle() {
       toggleButton.addEventListener('click', () => {
         navbarLinks.classList.toggle('active');
       });
-    } else {
-      console.error('Toggle button or nav list not found');
     }
   }
